e07bac8267751aafc3228ac348cfae96794e88db
Rehan Razaq <58367730+rehan509@users.noreply.github.com>
2022-07-21 23:42:55 +0500

    user data and disclaimer added

--- app/components/templates/footer.tsx
+++ app/components/templates/footer.tsx
@@ -26,7 +26,7 @@ const Footer = () => {
 
       <div className="my-2 h-[2px] w-[100%] border border-seacrest-150 bg-seacrest-150"></div>
 
-      <div className="flex flex-col items-center space-y-2 pt-[2rem] pb-[5rem] text-center md:flex-row md:items-center md:justify-between md:space-y-0 md:text-left">
+      <div className="flex flex-col items-center space-y-2 pt-[2rem]  text-center md:flex-row md:items-center md:justify-between md:space-y-0 md:text-left">
         <h2 className="text-base font-medium text-seacrest-200">© 2022 Seacrest Markets. All rights reserved.</h2>
         <div className="flex items-start justify-center gap-8 py-4">
           <img src="/Twitter.svg" alt="twiter" />
@@ -36,6 +36,7 @@ const Footer = () => {
           <img src="/Facebook.svg" alt="facebook" />
         </div>
       </div>
+      <h1 className='text-base pb-[5rem] font-medium text-seacrest-200'> <h2 >Risk Warning and Disclaimer :</h2>   Leveraged products such as CFD’s and Forex trading are complex instruments with a high risk of losing money. The products offered are intended for professional and retail clients. Please note that client accounts could sustain losses of deposited funds or in some cases even exceeding their deposit amount. <br/> Please note; The information on the website is not directed at any jurisdiction and is not intended for any use that would be contrary to local law or regulation. Seacrest Markets LLC is a registered business in Saint Vincent and Grenadines under BAN xxxxxxxxxx.</h1>
     </div>
   );
 };
